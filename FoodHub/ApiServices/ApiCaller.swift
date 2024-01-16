137c9f62321a179a259ae541d3c6d57d051e2d9a
Moataz1104 <moataz.mohamed.id@gmail.com>
2024-01-16 20:31:12 +0200

    Build Request and model for registration

--- FoodHub/ApiServices/ApiCaller.swift
+++ FoodHub/ApiServices/ApiCaller.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+
+
+
+
+
+import Foundation
+import RxSwift
+
+class ApiCaller {
+    
+    static let shared = ApiCaller()
+    
+    private init() {}
+    
+    private let baseUrl = URL(string: "https://5856-197-134-125-52.ngrok-free.app/api/Authentication")!
+    
+    
+    
+    
+    func registerUser(username: String, email: String, password: String) -> Observable<RegisterModel> {
+        let body: [String: Any] = [
+            "username": username,
+            "email": email,
+            "password": password,
+            "phoneNumber": "00000000000",
+            "roles": ["User"]
+        ]
+        return buildRequest(method: "POST", pathComponent: "/register", body: body)
+            .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+            .map { data in
+                try! JSONDecoder().decode(RegisterModel.self, from: data)
+            }.observe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+    }
+    
+    private func buildRequest(method: String, pathComponent: String, body: [String: Any]?) -> Observable<Data> {
+        let url = baseUrl.appendingPathComponent(pathComponent)
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        if method == "GET" {
+            // Handle GET request
+        } else {
+            let jsonData = try! JSONSerialization.data(withJSONObject: body!, options: .prettyPrinted)
+            request.httpBody = jsonData
+            
+        }
+        
+        request.url = url
+        request.httpMethod = method
+        
+        let session = URLSession.shared
+        
+        return session.rx.data(request: request)
+    }
+}
--- FoodHub/RegisterModel.swift
+++ FoodHub/RegisterModel.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct RegisterModel:Codable{
+    let status:String
+    let message:String
+}
