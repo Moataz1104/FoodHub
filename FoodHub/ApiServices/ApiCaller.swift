633973b2a039b9141cfc528a5879ebc94c9f42a4
Moataz1104 <moataz.mohamed.id@gmail.com>
2024-01-16 20:30:08 +0200

    Build Request and model for registration

---
+++ FoodHub/ApiServices/ApiCaller.swift
@@ -0,0 +1,8 @@
+//
+//  ApiCaller.swift
+//  FoodHub
+//
+//  Created by Moataz Mohamed on 15/01/2024.
+//
+
+import Foundation
---
+++ FoodHub/RegisterModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterModel.swift
+//  FoodHub
+//
+//  Created by Moataz Mohamed on 16/01/2024.
+//
+
+import Foundation
